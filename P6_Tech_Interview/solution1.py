26bf13acd0b0a14a66a67636de1d30514104a71b
Ramil Sharifsoy <ramilsharifsoy@users.noreply.github.com>
2017-07-22 21:34:33 -0700

    Daily Check-in July-22

--- P6_Tech_Interview/solution1.py
+++ P6_Tech_Interview/solution1.py
@@ -12,37 +12,53 @@ then the function returns True. Your function definition should look
 like: question1(s, t) and return a boolean True or False.
 """
 
-""" 
-Solution:  
-    1. Accept input as a string
-    2. Convert string to list and sort  those lists
+
+# Solution:
+
+"""
+First make sure that both phrases entered.
+ 
+With H Function, slice "s" into sections with legth of "t", feed slices of 
+"s" with "t" to A Function. Which will sort and compare each slice of "s" 
+with sorted "t".
+"""
+
+def question1(stringS, testT):                                 # H Function
+    if stringS == "" or testT == "":
+        return "Enter Both Words"      
+    else:  
+        testT_length = len(testT)
+        stringS_length = len(stringS)
+        for i in range(stringS_length - testT_length + 1):         # Limit counter to number of slices
+          # print (stringS[i: i+testT_length], testT)              # Print slices of stringS with testT
+            if is_anagrammer(stringS[i: i+testT_length], testT):   # End loop and return True if there is match
+                return True
+        return False
+
+"""  
+    1. Accept inputs as a strings
+    2. Convert string to list and sort those lists
     3. Compared sorted lists. Regardless of initial configuration of 
     letters, sorted lists will match if they are same.
 """
 
-def is_anagrammer(string1, string2):
+def is_anagrammer(string1, string2):                          # A Function
     string1_list = list(string1)
     string1_list.sort()
     string2_list = list(string2)
     string2_list.sort()
     return string1_list == string2_list
 
-"""
-Slice "s" into sections with legth of "t" and compare each sorted slice 
-of "s" with sorted "t". If there is match print True, if not False.
-"""
+# Test Code:
+    
+print (question1("udacity", "ad"))             # True
+print (question1("udacity", "ram"))            # False
+print (question1("udacity", " "))              # False
+print (question1("udacity", "acid"))           # True
+print (question1("udacity", ""))               # Enter Both Words
+print (question1("", "udacity"))               # Enter Both Words
+print (question1("udacity", "udacities"))      # t longer than s > False 
 
-def question1(stringS, testT):
-    testT_length = len(testT)
-    stringS_length = len(stringS)
-    for i in range(stringS_length - testT_length + 1):         # Limit counter to number of slices
-        print (stringS[i: i+testT_length], testT)              # Print slices of stringS with testT
-        if is_anagrammer(stringS[i: i+testT_length], testT):   # End loop and return True if there is match
-            return True
-    return False
-
-print (question1("udacity", "ad"))  # True
-print (question1("udacity", "ram")) # False
 
 """
 Reference: https://github.com/bradd123/udacity-technical-interview-practice/blob/master/1.py
--- P6_Tech_Interview/solution2.py
+++ P6_Tech_Interview/solution2.py
@@ -8,30 +8,36 @@ Created on Fri Jul 21 12:12:43 2017
 Given a string a, find the longest palindromic substring contained in a.
 Your function definition should look like question2(a), and return a string.
 """
+# Main function.
+def Question2(a):
+    a = a.lower()
+    a = a.replace(" ", "")
+    for l in substrings(a):
+        if palindrome(l):
+            return l
 
 # Gives substrings of s in decending order.
 def substrings(s):
-
+    
     # Declare local variable for the length of s.
-    l = len(s)
+    length_s = len(s)
 
     # Here I chose range over xrange for python version compatibility.
-    for end in range(l, 0, -1):
-        for i in range(l-end+1):
+    for end in range(length_s, 0, -1):
+        for i in range(length_s-end+1):
             yield s[i: i+end]
 
 # Define palindrome.
 def palindrome(s):
     return s == s[::-1]
 
-# Main function.
-def Question2(a):
-    for l in substrings(a):
-        if palindrome(l):
-            return l
-
 # Simple test case.
 print (Question2("I love bananas !!!"))
+print (Question2("I have a racecar !!!"))
+print (Question2("A nut for a jar of tuna."))
+print (Question2("Was it a car or a cat I saw?"))
+print (Question2("Murder for a jar of red rum."))
+print (Question2("Gateman sees name, garageman sees nametag."))
 
 """
 Reference: https://codereview.stackexchange.com/questions/160142/finding-the-longest-palindromic-substring
